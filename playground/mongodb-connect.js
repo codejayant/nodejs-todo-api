4b1f1a43a1862aa9108de7de50d22a30fd5730e9
jayant sarkar <codejayant@gmail.com>
2017-10-20 02:45:42 -0700

    first commit : basic setup, mongodb create collection and insert data example

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,34 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err)  {
+    return console.log('Unable to connect to MongoDB server : ' + err);
+  }
+
+  console.log('Connected to MongoDB server');
+
+  db.collection('Todos').insertOne({
+    text: 'Learn NodeJS',
+    completed: false
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert todo due to : ', err);
+    }
+
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
+  db.collection('Users').insertOne({
+    name: 'jayant',
+    age: 29,
+    location: 'san francisco'
+  }, (err, result) => {
+    if (err)  {
+      return console.log('Unable to insert user due to : ', err);
+    }
+
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
+  db.close();
+});
