955308a790cf257188114c50e07996c47c84a368
jayant sarkar <codejayant@gmail.com>
2017-10-23 02:32:41 -0700

    added user mongoose data model

--- server/server.js
+++ server/server.js
@@ -5,34 +5,48 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
-var newTodo = new Todo({
-  text: 'cook dinner'
+// var otherTodo = new Todo({
+//   text: 'cook dinner',
+//   completed: true,
+//   completedAt: 123
+// });
+
+// otherTodo.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('Unable to save todo');
+// });
+
+var User = mongoose.model('Users', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
 });
 
-newTodo.save().then((doc) => {
-  console.log('Saved todo', doc);
-}, (e) => {
-  console.log('Unable to save todo');
+var newUser = new User({
+  email: 'test@test.com'
 });
 
-var newTodo1 = new Todo({
-  text: 'cook dinner',
-  completed: true,
-  completedAt: 123
-});
-
-newTodo1.save().then((doc) => {
-  console.log('Saved todo', doc);
-}, (e) => {
-  console.log('Unable to save todo');
+newUser.save().then((doc) => {
+  console.log(doc);
+}, (err) => {
+  console.log('Unable to save new user due to : ', err);
 });
